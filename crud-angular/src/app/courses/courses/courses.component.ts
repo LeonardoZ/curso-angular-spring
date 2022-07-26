bc4ffa910a44c4b296825a2110aec56bbbdf7d60
Leonardo Zapparoli <leo.zapparoli@gmail.com>
2022-07-25 21:31:31 -0300

    Aula 19

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { catchError, Observable, of } from 'rxjs';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
@@ -13,10 +14,12 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
-  displayedColumns = ['_id', 'name', 'category'];
+  displayedColumns = ['_id', 'name', 'category', 'actions'];
 
   constructor(
     private coursesServices: CoursesService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
     public dialog: MatDialog
   ) {
     this.courses$ = this.coursesServices.list().pipe(
@@ -25,6 +28,11 @@ export class CoursesComponent implements OnInit {
         return of([]);
       })
     );
+  }
+
+  onAdd() {
+    console.log('onAdd');
+    this.router.navigate(['new'], { relativeTo: this.activatedRoute });
   }
 
   onError(errorMessage: string) {
